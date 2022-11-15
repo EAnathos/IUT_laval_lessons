8d0727319786f836a00cec8795fa0c985574ce59
EAnathos <eanathos@gmail.com>
2022-11-15 14:11:41 +0100

    interface and implementation of the class

--- TDA/src/main/java/pile/Pile.java
+++ TDA/src/main/java/pile/Pile.java
@@ -1,4 +1,15 @@
 package pile;
 
 public interface Pile {
+    boolean pileVide();
+
+    boolean pilePleine();
+
+    int longueur();
+
+    Object sommet();
+
+    void empiler();
+
+    Object depiler();
 }
---
+++ TDA/src/main/java/pile/PileParTableau.java
@@ -0,0 +1,33 @@
+package pile;
+
+public class PileParTableau implements Pile {
+    @Override
+    public boolean pileVide() {
+        return false;
+    }
+
+    @Override
+    public boolean pilePleine() {
+        return false;
+    }
+
+    @Override
+    public int longueur() {
+        return 0;
+    }
+
+    @Override
+    public Object sommet() {
+        return null;
+    }
+
+    @Override
+    public void empiler() {
+
+    }
+
+    @Override
+    public Object depiler() {
+        return null;
+    }
+}
