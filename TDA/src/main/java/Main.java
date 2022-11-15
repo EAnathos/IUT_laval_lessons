59dcc5f462a1ff92b7a743a74e7c40dcb311af32
EAnathos <eanathos@gmail.com>
2022-11-15 16:06:57 +0100

    add calculatrice

--- TDA/src/main/java/Main.java
+++ TDA/src/main/java/Main.java
@@ -1,11 +1,50 @@
+import java.util.Scanner;
 import pile.PileParTableau;
 
 import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
-        PileParTableau pile1 = new PileParTableau(5);
+        //PileParTableau pile1 = new PileParTableau(5);
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Combien de valeur voulez vous dans votre tableau ?");
+
+        int tabLength = scan.nextInt();
+
+        char[] tab = new char[tabLength];
+
+        int pileLength = 0;
+
+        for (int i=0; i<tabLength; i++) {
+            System.out.println("Entrez votre valeur dans le tableau, celà doit être un opérateur ou un chiffre");
+            tab[i] = scan.next().charAt(0);
+            if (isInteger(tab[i])) {
+                pileLength++;
+            }
+        }
+
+        System.out.println("Votre pile comporte " + pileLength + " nombres");
+
+        PileParTableau pile2 = new PileParTableau(pileLength);
+
+        for(int i=0; i<pileLength; i++) {
+            pile2.empiler(tab[i]);
+        }
+
+        int currentOp = pileLength;
+
+        while (pile2.longueur() != 1) {
+            int operator2 = Integer.parseInt(String.valueOf(pile2.depiler()));
+            int operator1 = Integer.parseInt(String.valueOf(pile2.depiler()));
+            char operator = tab[currentOp];
+            pile2.empiler(Operate(operator1, operator2, operator));
+            currentOp++;
+        }
+
+        System.out.println(pile2.sommet());
+
+        /*
         for (int i=0; i<5;i++) {
             pile1.empiler(i);
             Arrays.stream(pile1.myPile).forEach(o -> System.out.println(o));
@@ -15,6 +54,29 @@ public class Main{
 
         for (int i=0; i<5; i++) {
             System.out.println(pile1.depiler());
+        }
+         */
+    }
+
+    private static boolean isInteger(final char elem) {
+        try {
+            Integer.parseInt(String.valueOf(elem));
+            return true;
+        } catch (final NumberFormatException exception) {
+            return false;
+        }
+    }
+
+    private static int Operate(int elem1, int elem2, char op) {
+        switch (op) {
+            case '+':
+                return elem1 + elem2;
+            case '*':
+                return elem1 * elem2;
+            case '-':
+                return elem1 - elem2;
+            default:
+                return elem1 / elem2;
         }
     }
 }
