7e75b331bc3403fe93ad1de2ae71de732e2a8672
EAnathos <eanathos@gmail.com>
2022-12-05 16:18:18 +0100

    write the List in a binary file

--- collections/src/main/java/Ludotheque.java
+++ collections/src/main/java/Ludotheque.java
@@ -1,14 +1,28 @@
+import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Ludotheque {
     public static void main(String[] args) {
-        List<VideoGame> gameList = new LinkedList<VideoGame>();
+        List<VideoGame> gameList = new LinkedList<>();
         gameList.add(new VideoGame("Pacman", "Action/reflexion", "Atari Corporation", 1982, 18));
         gameList.add(new VideoGame("Tetris", "Puzzle", "Nintendo", 1984, 20));
         gameList.add(new VideoGame("2048", "Puzzle", "SoleBon LLC", 2014, 15));
 
         gameList.sort(VideoGame.compareName);
-        System.out.println(gameList);
+
+        gameList.forEach(System.out::println);
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream("test.dat");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(gameList);
+            out.close();
+            fileOut.close();
+            System.out.println("\nsuccess...\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- collections/src/main/java/VideoGame.java
+++ collections/src/main/java/VideoGame.java
@@ -1,25 +1,17 @@
+import java.io.Serializable;
 import java.util.Comparator;
 
 // A Class made for store information about video games.
-public class VideoGame {
+public class VideoGame implements Serializable {
     private final String name;
     private final String gender;
     private final String editor;
     private final int year;
     private final int grade;
 
-    static public Comparator<VideoGame> compareName = new Comparator <VideoGame>() {
-
-        public int compare(VideoGame jv1, VideoGame jv2) {
-
-            return jv1.name.compareToIgnoreCase(jv2.name);
-
-        }
-    };
-
-    // ++++++++++++++++++++++++++++++++++++++
-    // other comparators are created here
-    // ++++++++++++++++++++++++++++++++++++++
+    static public Comparator<VideoGame> compareName = (jv1, jv2) -> jv1.name.compareToIgnoreCase(jv2.name);
+    static public Comparator<VideoGame> compareGender = (jv1, jv2) -> jv1.gender.compareToIgnoreCase(jv2.gender);
+    static public Comparator<VideoGame> compareEditor = (jv1, jv2) -> jv1.editor.compareToIgnoreCase(jv2.editor);
 
     // A constructor to give an init value at the creation.
     VideoGame(String s, String g, String e, int a, int n)
@@ -37,6 +29,5 @@ public class VideoGame {
     {
         return ("Game : "+name+" ; Gender : "+gender+" ("+editor+", "+year+")"+" : "+grade);
     }
-
 
 }
